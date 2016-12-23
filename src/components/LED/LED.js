c0e4f255d33738dc25bb6c7c3431876c680ec6c2
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2016-12-23 14:30:48 +0100

    make LED stateless component

--- src/components/LED/LED.js
+++ src/components/LED/LED.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import FlatButton from 'material-ui/FlatButton';
 import LightbulbOutline from 'material-ui/svg-icons/action/lightbulb-outline';
 import {fullWhite, fullBlack} from 'material-ui/styles/colors';
 import Color from 'color';
 import './LED.css';
 
-
-class LED extends Component {
-  handleClick() {
-    if(this.props.onClick) {
-      this.props.onClick();
+const LED = ({color, onClick}) => {
+  const handleClick = () => {
+    if(onClick) {
+      onClick();
     }
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <FlatButton
-          onClick={() => {this.handleClick()}}
-          backgroundColor={this.props.color}
-          hoverColor={Color(this.props.color).darken(0.3).string()}
-          icon={<LightbulbOutline color={Color(this.props.color).dark() ? fullWhite : fullBlack} />}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <FlatButton
+        onClick={() => {handleClick()}}
+        backgroundColor={color}
+        hoverColor={Color(color).darken(0.3).string()}
+        icon={<LightbulbOutline color={Color(color).dark() ? fullWhite : fullBlack} />}
+      />
+    </div>
+  );
+};
 
 export default LED;
