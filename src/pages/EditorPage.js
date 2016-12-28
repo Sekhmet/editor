503c758e644da1c293917a855178608539e6b2a4
Wiktor Tkaczyński <wiktor.tkaczynski@gmail.com>
2016-12-28 19:30:51 +0100

    add fab to editor

--- src/pages/EditorPage.js
+++ src/pages/EditorPage.js
@@ -1,14 +1,25 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
 import ToolboxContainer from '../containers/ToolboxContainer';
 import FrameSelectorContainer from '../containers/FrameSelectorContainer';
 import LayerContainer from '../containers/LayerContainer';
+
+const FAB = styled(FloatingActionButton)`
+  position: fixed;
+  right: 16px;
+  bottom: 16px;
+`;
 
 const EditorPage = () => (
   <div>
     <ToolboxContainer />
     <FrameSelectorContainer />
     <LayerContainer />
+    <FAB>
+      <ContentAdd />
+    </FAB>
   </div>
 );
 
